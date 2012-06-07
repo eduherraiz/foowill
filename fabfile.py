d2b2cb4ea293229bef63a37547fcd20a5ad130a1
Edu Herraiz <gshark@gmail.com>
2012-06-07 02:20:00 +0200

    Fabfile using workon and functions to manage deploy

--- fabfile.py
+++ fabfile.py
@@ -4,11 +4,10 @@ env.use_ssh_config = True
 env.directory = '/mnt/xuflus/Webs/foowill'
 env.deploy_user = 'root'
 env.hosts = ['root@foowill.com']
-workon = "source /root/.virtualenvs/foowill/bin/activate && cd %s" % env.directory
 
 def syncdb():
-        run('python manage.py syncdb')
-        run('python manage.py migrate')
+    with cd(env.directory):
+        run('workon foowill && python manage.py syncdb && python manage.py migrate')
 
 def git_push():
     'Local push to the repository.'
@@ -18,18 +17,36 @@ def git_push():
 def git_pull():
     'Updates the repository.'
     with cd(env.directory):
-	run('git pull')
+    run('git pull')
+
+def stop_supervisor():
+    with cd(env.directory):
+        run("workon foowill && python manage.py supervisor stop all" )
         
-def run_webserver():
+def start_supervisor():
+    with cd(env.directory):
+        run("workon foowill && python manage.py supervisor --daemonize --project-dir=%s" % env.directory)
+    
+def stop_webserver():
     'Restart nginx and run the supervisor for celery, redis, anf gunicorn'
-    run("/etc/init.d/nginx restart")
-    with prefix(workon):
-	run("python manage.py supervisor")
+    run("/etc/init.d/nginx stop")
+    stop_supervisor()
         
+def start_webserver():
+    'Restart nginx and run the supervisor for celery, redis, anf gunicorn'
+    run("/etc/init.d/nginx start")
+    with cd(env.directory):
+        run("workon foowill && python manage.py supervisor --daemonize --project-dir=%s" % env.directory)
+        
+def restart_webserver():
+    stop_supervisor()
+    run("/etc/init.d/nginx restart")
+    start_supervisor()
+
 def deploy():
     git_push()
-    #server()
+    stop_webserver()
     git_pull()
-    #syncdb()
-    run_webserver()
+    syncdb()
+    start_webserver()
     
