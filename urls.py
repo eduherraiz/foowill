a7b1396f4e54983926975f1424bf0a18d9a5eb25
Edu Herraiz <gshark@gmail.com>
2012-07-25 21:20:53 +0200

    Auto push on deploy - small changes

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.utils.translation import ugettext_lazy as _
+from django.http import HttpResponse
 
 
 # Uncomment the next two lines to enable the admin:
@@ -10,6 +11,7 @@ from app.views import *
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
     url(r'', include('app.urls')),
     url(r'', include('social_auth.urls')),
     url(r'^i18n/', include('django.conf.urls.i18n')),
