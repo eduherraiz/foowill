c9618b38da3138e50232a210ae56c391e3a96f15
Edu Herraiz <gshark@gmail.com>
2012-10-09 21:34:56 +0200

    Added catala language. security message. Image background whited

--- settings.py
+++ settings.py
@@ -64,13 +64,14 @@ TIME_ZONE = 'Europe/Madrid'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-US'
+LANGUAGE_CODE = 'es-ES'
 
 #Function to prevent a circular import if use  django.utils.translation
 
 LANGUAGES = (
     ('en', 'English'),
     ('es', 'Español'),
+    ('ca', 'Català')
 )
 
 SITE_ID = 1
